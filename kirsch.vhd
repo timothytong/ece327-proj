d990f9d822fa376a0f0064f3e65f9e953ae038f2
John Xu <jx2xu@uwaterloo.ca>
2016-03-17 19:01:31 -0400

    Added memory access and convolution table registers, updated simulation script

--- kirsch.vhd
+++ kirsch.vhd
@@ -1,7 +1,13 @@
 library IEEE;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+
 entity kirsch is
+  generic (
+      data_width : natural := 8;
+      addr_width : natural := 8 
+  );
+
   port(
     ------------------------------------------
     -- main inputs and outputs
@@ -32,7 +38,185 @@ end entity;
 
 
 architecture main of kirsch is
+
+-- Functions and types
+-- A function to rotate left (rol) a vector by n bits
+function "rol" ( a : std_logic_vector; n : natural )
+    return std_logic_vector
+is
 begin
+    return std_logic_vector( unsigned(a) rol n );
+end function;
+
+subtype vec is std_logic_vector(data_width - 1 downto 0);
+type vec_vec is array (1 downto 0) of vec;
+
+-- Control signals (nets)
+signal mem_wren : std_logic_vector(0 to 1);
+--signal p : signed(9 downto 0); -- P needs 10 bits to accomodate the range -255 to 510
+--signal a_data, b_data, c_data : unsigned(data_width - 1 downto 0);
+
+-- State signals (registered)
+signal read : std_logic;
+signal just_read : std_logic;
+signal column : unsigned(addr_width - 1 downto 0 );
+signal row : std_logic_vector(1 downto 0 );
+signal row_virtual : unsigned(addr_width - 1 downto 0);
+signal completed : std_logic;
+signal count : unsigned(7 downto 0);
+signal valid : std_logic_vector(7 downto 0);
+
+--Memory outputs (registered)
+signal mem_out: vec_vec;
+signal a, b, c, d, e, f, g, h, i : unsigned(data_width - 1 downto 0);
+signal a_buffer, b_buffer, c_buffer: std_logic_vector(data_width - 1 downto 0);
+begin
+
+
+--combinational assignments (net signals)
+    mem_wren(0) <= row(0) and i_valid;
+    mem_wren(1) <= row(1) and i_valid;
+--    mem_wren(2) <= row(2) and i_valid;
+
+    -- Memory arrays
+    memory_row_1 : entity work.mem(main)
+    port map(
+                address => std_logic_vector(column),
+                clock => i_clock,
+                data => std_logic_vector(i_pixel),
+                wren => mem_wren(0),
+                q => mem_out(0)
+            );
+
+    memory_row_2 : entity work.mem(main)
+    port map(
+                address => std_logic_vector(column),
+                clock => i_clock,
+                data => std_logic_vector(i_pixel),
+                wren => mem_wren(1),
+                q => mem_out(1)
+            );
+
+--    memory_row_3 : entity work.mem(main)
+--    port map(
+--                address => std_logic_vector(column),
+--                clock => clock,
+--                data => std_logic_vector(i_data),
+--                wren => mem_wren(2),
+--                q => mem_out(2)
+--            );
+    buffers : process (i_clock)
+    begin
+        if rising_edge(i_clock) then
+            if i_reset = '1' then
+                a_buffer <= (others => '0');
+                b_buffer <= (others => '0');
+                c_buffer <= (others => '0');
+                
+                h <= (others => '0');
+                i <= (others => '0');
+                d <= (others => '0');
+                
+                g <= (others => '0');
+                f <= (others => '0');
+                e <= (others => '0');
+
+
+            else
+                if i_valid  = '1' then
+                    if row(0) = '1' then
+                        c_buffer <= mem_out(0);
+                        d <= unsigned(mem_out(1));
+                    else
+                        c_buffer <= mem_out(1);
+                        d <= unsigned(mem_out(0));
+                    end if;
+
+                    b_buffer <= c_buffer;
+                    a_buffer <= b_buffer;
+
+                    i <= d;
+                    h <= i;
+
+                    e <= unsigned(i_pixel);
+                    f <= e;
+                    g <= f;
+                end if;
+            end if;
+        end if;
+    end process;
+ 
+    states : process(i_clock)
+    begin
+        if rising_edge(i_clock) then
+            if i_reset = '1' then
+                column <= to_unsigned(0, addr_width);
+                row <= "01";
+                --read <= '0';
+                --just_read <= '0';
+                row_virtual <= to_unsigned(0, addr_width);
+            else
+                --if read = '1' then
+                --    read <= '0';
+                --end if;
+
+                if valid(0) = '1' then
+                    if column = to_unsigned(255, 8) then
+                        column <= to_unsigned(0, 8);
+                        row <= "rol"(row, 1);
+                        row_virtual <= row_virtual + 1;
+                    else
+                        column <= column + 1;
+                    end if;
+
+                --elsif i_valid = '1' then
+                --    read <= '1';
+                --end if;
+
+                --just_read <= read;
+                end if;
+            end if;
+        end if;
+    end process;
+
+--  populate_convolution_table : process (i_clock)
+--  begin
+--      if rising_edge(i_clock) then
+--          if i_reset = '1' then
+--              h <= to_unsigned(0, 8);
+--              i <= to_unsigned(0, 8);
+--              d <= to_unsigned(0, 8);
+--          else
+--              if row_virtual >= 2  then
+--                  if column >= 2 then
+--                      --a <= mem_row
+--                  elsif i_valid = '1' then
+--                      if column = 0 then
+--                          g <= unsigned(i_pixel);
+--                      elsif column = 1 then
+--                          f <= unsigned(i_pixel);
+--                      end if;
+--                  end if;
+--              end if;
+--          end if;
+--      end if;
+--  end process;
+
+
+
+  valid_bit : process(i_clock)
+  begin 
+      if rising_edge(i_clock) then
+          if i_reset = '1' then
+              valid <= (others => '0');
+          else
+              -- logical shift right, bring new i_valid to beginning of valid bit chain
+              --valid <= valid sll 1;
+              --valid(0) <= i_valid;
+              valid <= valid(6 downto 0) & i_valid;
+          end if;
+      end if;
+  end process;
 
   debug_num_5 <= X"E";
   debug_num_4 <= X"C";
