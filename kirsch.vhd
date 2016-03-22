b6c346fdab6a9d2361b5d03f46efcb57f8dfc061
John Xu <jx2xu@uwaterloo.ca>
2016-03-22 03:05:58 -0400

    Added pipeline stage 2, moved custom modules to top of file to fix compile dependency

--- kirsch.vhd
+++ kirsch.vhd
@@ -1,3 +1,114 @@
+--------------------------------------------------------------------------------------------------
+--------------------------------- CUSTOM MODULES -------------------------------------------------
+--------------------------------------------------------------------------------------------------
+
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity custom_max is
+    generic (
+        width : natural := 8
+    );
+    port ( i_pix1, i_pix2  :  in unsigned (width - 1 downto 0);         -- input will be 8 bits
+           i_dir1, i_dir2 :  in std_logic_vector (2 downto 0);  -- directions are encoded from 000 (W) to 111 (SW)
+           o_max_dir      : out std_logic_vector (2 downto 0);
+           o_max_pix       : out unsigned (width - 1 downto 0)
+    );
+end custom_max;
+
+architecture main of custom_max is
+
+begin
+    -- o_max_val takes max(i_dir1, i_dir2) just like all other maxes
+    -- o_max_dir takes the direction of max(i_dir1, i_dir2)
+    -- if i_pix1 == i_pix2, take i_pix1 and i_dir1 due to priority stated in lab manual
+
+    o_max_pix <= i_pix1 when ( i_pix1 >= i_pix2 ) else i_pix2;
+    o_max_dir <= i_dir1 when ( i_pix1 >= i_pix2 ) else i_dir2;
+
+end architecture main;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Stage 1 hardware consists of 1 8-bit max and 2 9-bit adders (m1, a1, and a2 in DFD)
+-- The max module is custom-made, implemented in custom_max.vhd
+entity stage1_hardware is
+    port ( i_dir1_stage1, i_dir2_stage1        :  in std_logic_vector ( 2 downto 0 );
+           i_pix1_stage1, i_pix2_stage1        :  in unsigned ( 7 downto 0 );
+           i_add_op1_stage1, i_add_op2_stage1  :  in unsigned ( 7 downto 0 );
+           o_add_op12_stage1, o_max_add_stage1 : out unsigned ( 8 downto 0 );
+           o_max_dir_stage1                    : out std_logic_vector ( 2 downto 0 )
+    );
+end stage1_hardware;
+
+architecture main of stage1_hardware is
+    signal custom_max_pix_output : unsigned ( 7 downto 0 );
+    signal sum_a1_a2             : unsigned ( 8 downto 0 );
+begin
+    -- instantiate custom max module
+    u_max : entity work.custom_max(main)
+        generic map (
+            width => 8
+        )
+        port map (
+            i_dir1 => i_dir1_stage1,
+            i_dir2 => i_dir2_stage1,
+            i_pix1 => i_pix1_stage1,
+            i_pix2 => i_pix2_stage1,
+            o_max_dir => o_max_dir_stage1,
+            o_max_pix => custom_max_pix_output
+        );
+
+    sum_a1_a2 <= resize(i_add_op1_stage1, 9) + resize(i_add_op2_stage1, 9);
+    o_add_op12_stage1 <= sum_a1_a2;
+    o_max_add_stage1 <= custom_max_pix_output + sum_a1_a2;
+end architecture main;
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+-- Stage 2 hardware consists of 1 9-bit max and 1 9-bit adder (m2 and a3 in DFD)
+-- The max module is custom-made, implemented in (custom_max_9bit.vhd?????)
+entity stage2_hardware is
+    port ( i_dir1_stage2, i_dir2_stage2        :  in std_logic_vector ( 2 downto 0 );
+           i_pix1_stage2, i_pix2_stage2        :  in unsigned ( 8 downto 0 );
+           i_add_op1_stage2, i_add_op2_stage2  :  in unsigned ( 12 downto 0 );
+           o_add_op12_stage2                   : out unsigned ( 12 downto 0 );
+           o_max_stage2                        : out unsigned ( 8 downto 0 ); 
+           o_max_dir_stage2                    : out std_logic_vector ( 2 downto 0 )
+    );
+end stage2_hardware;
+
+architecture main of stage2_hardware is
+begin
+    -- instantiate custom max module
+    u_max : entity work.custom_max(main)
+        generic map (
+            width => 9
+        )
+        port map (
+            i_dir1 => i_dir1_stage2,
+            i_dir2 => i_dir2_stage2,
+            i_pix1 => i_pix1_stage2,
+            i_pix2 => i_pix2_stage2,
+            o_max_dir => o_max_dir_stage2,
+            o_max_pix  => o_max_stage2 
+        );
+
+    o_add_op12_stage2 <= resize(i_add_op1_stage2, 13) + resize(i_add_op2_stage2, 13);
+end architecture main;
+
+
+--------------------------------------------------------------------------------------------------
+--------------------------------- TOP LEVEL ENTITY KIRSCH ----------------------------------------
+--------------------------------------------------------------------------------------------------
+
+
 library IEEE;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -81,6 +192,17 @@ signal o_dir1 : std_logic_vector(2 downto 0);
 signal r1 : std_logic_vector(2 downto 0);
 signal r2, r3 : unsigned(data_width downto 0);
 
+-- Pipeline 2 combinational signals
+signal i_max3, i_max4 : unsigned(8 downto 0);
+signal i_dir3, i_dir4 : std_logic_vector(2 downto 0);
+signal i_add3, i_add4 : unsigned(12 downto 0);
+signal o_max1 : unsigned(8 downto 0);
+signal o_dir2 : std_logic_vector(2 downto 0);
+signal o_add3 : unsigned(12 downto 0);
+-- Pipeline 2 registered outputs
+signal r4 : unsigned(8 downto 0);
+signal r5 : std_logic_vector(2 downto 0);
+signal r6 : unsigned(12 downto 0);
 begin
 
 ------------------------------------------------------------------------------------------------- 
@@ -239,8 +361,8 @@ begin
               "111"; -- else SW
 
 
-    -- registered outputs r1 and r2
-    registers : process(i_clock)
+    -- registered outputs r1, r2, and r3
+    registers_stage_1 : process(i_clock)
     begin
         if rising_edge(i_clock) then
             --if i_reset = '1' then
@@ -253,6 +375,50 @@ begin
         end if;
     end process;
 
+    -- Pipeline 2
+    -- main reusable block instantiation
+    stage2 : entity work.stage2_hardware(main)
+    port map (
+           i_dir1_stage2 => i_dir3,
+           i_dir2_stage2 => i_dir4,
+           i_pix1_stage2 => i_max3,
+           i_pix2_stage2 => i_max4,
+           i_add_op1_stage2 => i_add3,
+           i_add_op2_stage2 => i_add4,
+           o_add_op12_stage2 => o_add3,
+           o_max_stage2 => o_max1,
+           o_max_dir_stage2 => o_dir2
+    );
+ 
+    -- control circuitry for stage 2
+    i_dir3 <= r1;
+    i_dir4 <= r5;
+    i_max3 <= r3;
+    i_max4 <= r4;
+    i_add3 <= (r6 sll 1) when valid(5) = '1' else
+              resize(r2, 13);
+    i_add4 <= r6;
+
+    -- registered outputs r4, r5 and r6
+    registers_stage_2 : process(i_clock)
+    begin
+        if rising_edge(i_clock) then
+            if valid(1) = '1' then
+                r4 <= r3;
+                r5 <= r1;
+                r6 <= resize(r2, 13);
+            elsif valid(5) = '1' then
+                r4 <= r4;
+                r5 <= r5;
+                r6 <= o_add3;
+            else
+                r4 <= o_max1;
+                r5 <= o_dir2;
+                r6 <= o_add3;
+            end if;
+        end if;
+    end process; 
+    -- Debugging
     debug_num_5 <= X"E";
     debug_num_4 <= X"C";
     debug_num_3 <= X"E";
@@ -264,110 +430,5 @@ begin
     debug_led_grn <= (others => '0');
 
 end architecture;
-
---------------------------------------------------------------------------------------------------
---------------------------------- CUSTOM MODULES -------------------------------------------------
---------------------------------------------------------------------------------------------------
-
-library IEEE;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
-entity custom_max is
-    generic (
-        width : natural := 8
-    );
-    port ( i_pix1, i_pix2  :  in unsigned (width - 1 downto 0);         -- input will be 8 bits
-           i_dir1, i_dir2 :  in std_logic_vector (2 downto 0);  -- directions are encoded from 000 (W) to 111 (SW)
-           o_max_dir      : out std_logic_vector (2 downto 0);
-           o_max_pix       : out unsigned (width - 1 downto 0)
-    );
-end custom_max;
-
-architecture main of custom_max is
-
-begin
-    -- o_max_val takes max(i_dir1, i_dir2) just like all other maxes
-    -- o_max_dir takes the direction of max(i_dir1, i_dir2)
-    -- if i_pix1 == i_pix2, take i_pix1 and i_dir1 due to priority stated in lab manual
-
-    o_max_pix <= i_pix1 when ( i_pix1 >= i_pix2 ) else i_pix2;
-    o_max_dir <= i_dir1 when ( i_pix1 >= i_pix2 ) else i_dir2;
-
-end architecture main;
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
--- Stage 1 hardware consists of 1 8-bit max and 2 9-bit adders (m1, a1, and a2 in DFD)
--- The max module is custom-made, implemented in custom_max.vhd
-entity stage1_hardware is
-    port ( i_dir1_stage1, i_dir2_stage1        :  in std_logic_vector ( 2 downto 0 );
-           i_pix1_stage1, i_pix2_stage1        :  in unsigned ( 7 downto 0 );
-           i_add_op1_stage1, i_add_op2_stage1  :  in unsigned ( 7 downto 0 );
-           o_add_op12_stage1, o_max_add_stage1 : out unsigned ( 8 downto 0 );
-           o_max_dir_stage1                    : out std_logic_vector ( 2 downto 0 )
-    );
-end stage1_hardware;
-
-architecture main of stage1_hardware is
-    signal custom_max_pix_output : unsigned ( 7 downto 0 );
-    signal sum_a1_a2             : unsigned ( 8 downto 0 );
-begin
-    -- instantiate custom max module
-    u_max : entity work.custom_max(main)
-        generic map (
-            width => 8
-        )
-        port map (
-            i_dir1 => i_dir1_stage1,
-            i_dir2 => i_dir2_stage1,
-            i_pix1 => i_pix1_stage1,
-            i_pix2 => i_pix2_stage1,
-            o_max_dir => o_max_dir_stage1,
-            o_max_pix => custom_max_pix_output
-        );
-
-    sum_a1_a2 <= resize(i_add_op1_stage1, 9) + resize(i_add_op2_stage1, 9);
-    o_add_op12_stage1 <= sum_a1_a2;
-    o_max_add_stage1 <= custom_max_pix_output + sum_a1_a2;
-end architecture main;
-
-
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-
--- Stage 2 hardware consists of 1 9-bit max and 1 9-bit adder (m2 and a3 in DFD)
--- The max module is custom-made, implemented in (custom_max_9bit.vhd?????)
-entity stage2_hardware is
-    port ( i_dir1_stage2, i_dir2_stage2        :  in std_logic_vector ( 2 downto 0 );
-           i_pix1_stage2, i_pix2_stage2        :  in unsigned ( 7 downto 0 );
-           i_add_op1_stage2, i_add_op2_stage2  :  in unsigned ( 8 downto 0 );
-           o_add_op12_stage2, o_max_add_stage2 : out unsigned ( 8 downto 0 );
-           o_max_dir_stage2                    : out std_logic_vector ( 2 downto 0 )
-    );
-end stage2_hardware;
-
-architecture main of stage2_hardware is
-    signal custom_max_pix_output : unsigned ( 7 downto 0 );
-begin
-    -- instantiate custom max module
-    u_max : entity work.custom_max(main)
-        generic map (
-            width => 9
-        )
-        port map (
-            i_dir1 => i_dir1_stage2,
-            i_dir2 => i_dir2_stage2,
-            i_pix1 => i_pix1_stage2,
-            i_pix2 => i_pix2_stage2,
-            o_max_dir => o_max_dir_stage2,
-            o_max_pix  => custom_max_pix_output
-        );
-
-    o_add_op12_stage2 <= resize(i_add_op1_stage2, 9) + resize(i_add_op2_stage2, 9);
-end architecture main;
 
 
