e34022096287cd335f566ed428d39d33b4881871
John Xu <jx2xu@uwaterloo.ca>
2016-03-23 17:55:47 -0400

    no data in signals

--- kirsch.vhd
+++ kirsch.vhd
@@ -24,8 +24,16 @@ begin
     -- o_max_dir takes the direction of max(i_dir1, i_dir2)
     -- if i_pix1 == i_pix2, take i_pix1 and i_dir1 due to priority stated in lab manual
 
-    o_max_pix <= i_pix1 when ( i_pix1 >= i_pix2 ) else i_pix2;
-    o_max_dir <= i_dir1 when ( i_pix1 >= i_pix2 ) else i_dir2;
+    process(i_pix1, i_pix2, i_dir1, i_dir2)
+    begin
+        if (i_pix1 >= i_pix2) then
+            o_max_pix <= i_pix1; --when ( i_pix1 >= i_pix2 ) else i_pix2;
+            o_max_dir <= i_dir1; --when ( i_pix1 >= i_pix2 ) else i_dir2;
+        else
+            o_max_pix <= i_pix2;
+            o_max_dir <= i_dir2;
+        end if;
+    end process;
 
 end architecture main;
 
@@ -64,7 +72,7 @@ begin
 
     sum_a1_a2 <= resize(i_add_op1_stage1, 9) + resize(i_add_op2_stage1, 9);
     o_add_op12_stage1 <= sum_a1_a2;
-    o_max_add_stage1 <= custom_max_pix_output + sum_a1_a2;
+    o_max_add_stage1 <= resize(custom_max_pix_output, 9) + sum_a1_a2;
 end architecture main;
 
 
@@ -164,20 +172,17 @@ type vec_vec is array (1 downto 0) of vec;
 
 -- Control signals (nets)
 signal mem_wren : std_logic_vector(0 to 1);
---signal p : signed(9 downto 0); -- P needs 10 bits to accomodate the range -255 to 510
---signal a_data, b_data, c_data : unsigned(data_width - 1 downto 0);
+signal mode : std_logic_vector(1 downto 0);
 
 -- State signals (registered)
-signal read : std_logic;
-signal just_read : std_logic;
 signal column : unsigned(addr_width - 1 downto 0);
-signal row : std_logic_vector(1 downto 0 );
 signal row_virtual : unsigned(addr_width - 1 downto 0);
-signal completed : std_logic;
-signal count : unsigned(7 downto 0);
-signal valid : std_logic_vector(7 downto 0);
+signal valid : std_logic_vector(6 downto 0);
 
-signal column_valid : unsigned(addr_width - 1 downto 0);
+signal column_valid1 : unsigned(addr_width - 1 downto 0);
+signal column_valid2 : unsigned(addr_width - 1 downto 0);
+signal row_valid1 : unsigned(addr_width - 1 downto 0);
+signal row_valid2 : unsigned(addr_width - 1 downto 0);
 
 --Memory outputs (registered)
 signal mem_out: vec_vec;
@@ -211,7 +216,9 @@ signal r4 : unsigned(8 downto 0);
 signal r5 : std_logic_vector(2 downto 0);
 signal r6 : unsigned(12 downto 0);
 
+
 -- Remaining parts of the pipeline
+signal sub_out : signed(13 downto 0);
 signal r7 : signed(13 downto 0);          -- Register for subtractor
 signal r8 : std_logic_vector(2 downto 0);
 signal r9 : std_logic;
@@ -223,10 +230,30 @@ begin
 -------------------------------- MEMORY AND CONVOLUTION TABLES ----------------------------------
 -------------------------------------------------------------------------------------------------
 
+    mode_logic : process(i_clock)
+    begin
+        if rising_edge(i_clock) then
+            if i_reset = '1' then
+                mode <= "01";
+            else
+                if mode = "01" then
+                    mode <= "10";
+                end if;
+                if i_valid = '1' then
+                    mode <= "11";
+                elsif (row_valid2 = 255 and column_valid2 = 255 and output_valid_reg = '1') then
+                    mode <= "10";
+                end if;
+            end if;
+        end if;
+    end process;
+                     
+    o_mode <= mode;
+    o_row <= std_logic_vector(row_virtual);
 
     --combinational assignments (net signals)
-    mem_wren(0) <= row(0) and i_valid;
-    mem_wren(1) <= row(1) and i_valid;
+    mem_wren(0) <= not row_virtual(0) and i_valid;
+    mem_wren(1) <= row_virtual(0) and i_valid;
 
     -- Memory arrays
     memory_row_1 : entity work.mem(main)
@@ -268,7 +295,7 @@ begin
 
             else
                 if i_valid  = '1' then
-                    if row(0) = '1' then
+                    if row_virtual(0) = '0' then
                         c <= unsigned(mem_out(0));
                         d <= unsigned(mem_out(1));
                     else
@@ -296,13 +323,13 @@ begin
         if rising_edge(i_clock) then
             if i_reset = '1' then
                 column <= to_unsigned(0, addr_width);
-                row <= "01";
+                --row <= "01";
                 row_virtual <= to_unsigned(0, addr_width);
             else
                 if valid(0) = '1' then
                     if column = to_unsigned(255, 8) then
                         column <= to_unsigned(0, 8);
-                        row <= "rol"(row, 1);
+                        --row <= "rol"(row, 1);
                         row_virtual <= row_virtual + 1;
                     else
                         column <= column + 1;
@@ -317,7 +344,12 @@ begin
     begin
         if rising_edge(i_clock) then
             if valid(0) = '1' then
-                column_valid <= column;
+                column_valid1 <= column;
+                row_valid1 <= row_virtual;
+            end if;
+            if valid(4) = '1' then
+                column_valid2 <= column_valid1;
+                row_valid2 <= row_valid1;
             end if;
         end if;
     end process;
@@ -330,7 +362,7 @@ begin
                 valid <= (others => '0');
             else
                 -- logical shift right, bring new i_valid to beginning of valid bit chain
-                valid <= valid(6 downto 0) & i_valid;
+                valid <= valid(5 downto 0) & i_valid;
             end if;
         end if;
     end process;
@@ -458,16 +490,18 @@ begin
         end if;
     end process;
 
+    sub_out <= signed(resize(r4, 14) sll 3) - signed(resize(r6, 14));
+
     -- comparator + output
     registers_stage_3 : process(i_clock)
     begin
         if rising_edge(i_clock) then
-            r7 <= signed(resize(r4, 14) sll 3) - signed(resize(r6, 14));
-
+            --r7 <= signed(resize(r4, 14) sll 3) - signed(resize(r6, 14));
+            
             if valid(6) = '1' then
                 r8 <= r5;
-            elsif valid(7) = '1' then -- TODO: also check for edge case at the BORDER
-                if r7 >= 383 then
+            --elsif valid(7) = '1' then -- TODO: also check for edge case at the BORDER
+                if sub_out >= 383 then
                     r9 <= '1';
                 else
                     r9 <= '0';
@@ -475,13 +509,14 @@ begin
             end if;
         end if;
     end process;
-    o_dir <= r8;
+    o_dir <= r8 when r9 = '1' else
+             "000";
     o_edge <= r9;
     --o_valid <= '1' when valid(7) = '1' and row_virtual >= 2 and (column >= 4 or column <= 2) else
     output_valid : process(i_clock)
     begin
         if rising_edge(i_clock) then
-            if (valid(7) = '1' and row_virtual >= 2 and column_valid >= 2) then
+            if (valid(6) = '1' and row_valid2 >= 2 and column_valid2 >= 2) then
                 output_valid_reg <= '1';
             else
                 output_valid_reg <= '0';
